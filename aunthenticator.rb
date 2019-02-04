61046d8aca02dfbc08ad469d22e18050410c7dc4
Roger <rdpender3@gmail.com>
2019-02-04 13:10:01 -0800

    starting layout

---
+++ aunthenticator.rb
@@ -0,0 +1,30 @@
+users = [
+    { username: "roger", password: "pender"},
+    { username: "jon", password: "snow"},
+    { username: "hatethe", password: "patriots"},
+    { username: "super", password: "bowl"},
+    { username: "wow", password: "rams"}
+]
+
+puts "This is my simple authenticator"
+25.times { print "-" }
+puts
+puts "Please enter a valid username and password"
+
+attempts = 1
+
+while attempts < 4
+
+print "Username: "
+username = gets.chomp
+
+print "Password: "
+password = gets.chomp
+
+puts "Press n to quit or any other key to continue: "
+input = gets.chomp.downcase
+
+break if input == "n"
+
+attempts += 1
+end
